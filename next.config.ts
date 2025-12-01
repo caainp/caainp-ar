b377f40efd08488bb549117597e70bf0e66fb34e
windopper <open0916@naver.com>
2025-12-01 20:54:46 +0900

    feat: Add API rewrites for development and production environments
    Implemented rewrites in next.config.ts to route API requests to a local server during development and to the production API in other environments. This enhancement improves the development workflow by allowing easier testing of API endpoints.

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,16 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "10mb",
     },
   },
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === 'development'
+        ? 'http://127.0.0.1:5328/api/:path*'
+        : '/api/',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
